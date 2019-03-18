9a3b1239741f6239897ebdb31d24c9889eb34a8d
Decio <dquintas@simonoop.com>
2019-03-18 10:31:11 +0000

    removed npm and started using exec

--- cli.js
+++ cli.js
@@ -3,6 +3,10 @@ const { version } = require('./package.json'),
     helper = require('./lib/helper');
 
 console.log(`Grunt me ${version}`);
+console.log('This will npm i the modules you are loading in the Gruntfile.')
 
 helper.readGruntFile()
     .then(helper.installModules)
+    .catch((err)=>{
+        console.log(`# Error:${err}`);
+    });
