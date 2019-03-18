e1e7a860b12f028fc3591e73fd41c72ada8e9f99
Decio <dquintas@simonoop.com>
2019-03-18 12:10:36 +0000

    add spinner and docs

--- cli.js
+++ cli.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 const { version } = require('./package.json'),
-    helper = require('./lib/helper');
+    helper = require('./lib/helper'),
+    [, , param] = process.argv;
 
 console.log(`Grunt me ${version}`);
-console.log('This will npm i the modules you are loading in the Gruntfile.')
 
-helper.readGruntFile()
-    .then(helper.installModules)
-    .catch((err)=>{
-        console.log(`# Error:${err}`);
-    });
+if (!['-S', '-D', '--save', '--save-dev'].includes(param)) {
+    console.log(`Usage:`);
+    console.log('gruntme -S         install to dependencies');
+    console.log('gruntme --save     install to dependencies');
+    console.log('gruntme -D         install to devDependencies');
+    console.log('gruntme --save-dev install to devDependencies');
+} else {
+
+    helper.readGruntFile()
+        .then(helper.installModules.bind(null, param))
+        .then(console.log.bind())
+        .catch(console.log.bind(null, `error:`));
+}
