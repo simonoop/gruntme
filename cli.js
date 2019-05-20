84142580cfd746fe311c6dada32523fe079d0604
Decio Quintas <decio.quintas@theaa.com>
2019-05-20 14:26:43 +0100

    adding sample grunt file. I am lazy.

--- cli.js
+++ cli.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 const { version } = require('./package.json'),
-    helper = require('./lib/helper'),
+    parser = require('./lib/parser'),
+    creator = require('./lib/creator'),
     [, , param] = process.argv;
 
 console.log(`Grunt me ${version}`);
 
-if (!['-S', '-D', '--save', '--save-dev'].includes(param)) {
+if (['-g'].includes(param)) {
+    creator.generateGruntFile()
+        .then(console.log)
+} else if (['-S', '-D', '--save', '--save-dev'].includes(param)) {
+    parser.readGruntFile()
+        .then(parser.installModules.bind(null, param))
+        .then(console.log.bind())
+        .catch(console.log.bind(null, `error:`));
+} else {
     console.log(`Usage:`);
+    console.log('gruntme            show these instructions');
     console.log('gruntme -S         install to dependencies');
     console.log('gruntme --save     install to dependencies');
     console.log('gruntme -D         install to devDependencies');
     console.log('gruntme --save-dev install to devDependencies');
-} else {
-
-    helper.readGruntFile()
-        .then(helper.installModules.bind(null, param))
-        .then(console.log.bind())
-        .catch(console.log.bind(null, `error:`));
+    console.log('gruntme -g         generate a Gruntfile.js.sample file');
 }
